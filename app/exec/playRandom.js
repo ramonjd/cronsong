ae72057fd71eaf41e7f2f3725245c40c12283276
Dodd, Ramon <ramon.dodd@bskyb.com>
2015-06-11 16:29:24 +0100

    create cron api

--- app/exec/playRandom.js
+++ app/exec/playRandom.js
@@ -12,7 +12,20 @@ var _configEnvDev = require('../config/env/dev');
 
 var _configEnvDev2 = _interopRequireDefault(_configEnvDev);
 
-var files = (0, _utilsUtils.filterMp3)(_fs2['default'].readdirSync(_configEnvDev2['default'].DIR));
-var randomIndex = (0, _utilsUtils.ran)(1, files.length);
-var song = (0, _utilsUtils.filterByIndex)(files, randomIndex - 1);
-(0, _utilsUtils.execute)(_configEnvDev2['default'].CMD + ' ' + _configEnvDev2['default'].DIR + song[0]);
+var song = null;
+var args = process.argv.slice(2);
+
+if (args[0]) {
+
+  if (args[0] === 'random') {
+    var files = (0, _utilsUtils.filterMp3)(_fs2['default'].readdirSync(_configEnvDev2['default'].DIR));
+    var randomIndex = (0, _utilsUtils.ran)(1, files.length);
+    song = (0, _utilsUtils.filterByIndex)(files, randomIndex - 1)[0];
+  } else {
+    song = args[0];
+  }
+  console.log(_configEnvDev2['default'].CMD + ' ' + _configEnvDev2['default'].DIR + song);
+  //execute(config.CMD + ' ' + config.DIR + song);
+} else {
+  console.log('No song argument passed');
+}
--- src/exec/playRandom.es6
+++ src/exec/playRandom.es6
@@ -5,8 +5,24 @@ import {filterByIndex} from '../utils/Utils';
 import {execute} from '../utils/Utils';
 import config from '../config/env/dev';
 
+let song = null;
+let args = process.argv.slice(2);
 
-let files = filterMp3(fs.readdirSync(config.DIR));
-let randomIndex = ran(1, files.length);
-let song = filterByIndex(files, randomIndex - 1);
-execute(config.CMD + ' ' + config.DIR + song[0]);
+if (args[0]) {
+  
+  if (args[0] === 'random') {
+    let files = filterMp3(fs.readdirSync(config.DIR));
+    let randomIndex = ran(1, files.length);
+    song = filterByIndex(files, randomIndex - 1)[0];
+  } else {
+    song = args[0];
+  }
+  console.log(config.CMD + ' ' + config.DIR + song);
+  //execute(config.CMD + ' ' + config.DIR + song);
+  
+  
+} else {
+  console.log('No song argument passed');
+}
+
+
